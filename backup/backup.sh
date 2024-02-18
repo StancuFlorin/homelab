8d115a1b53bac8b65467007cc6fdbce5b8c17a61
Stancu Florin <stancu.florin23@gmail.com>
2024-02-18 18:36:11 +0000

    updated backup script to keep the pull path of source folder

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,19 +1,47 @@
 #!/bin/bash
 
+# Read configuration from sync_config.conf
 source sync_config.conf
 
-if [ ! -d "$DESTINATION_PATH" ]; then
-    echo "Destination path does not exist. Please create it first."
-    exit 1
-fi
+# Function to get folder size
+get_folder_size() {
+    du -s "$1" | awk '{print $1}'
+}
 
+# Function to get size of destination path
+get_destination_size() {
+    du -s "$DESTINATION_PATH" | awk '{print $1}'
+}
+
+# Get size of destination path before sync
+destination_size_before=$(get_destination_size)
+
+# Perform the sync using rsync
 for path in "${SOURCE_PATHS[@]}"
 do
-    rsync -avz --progress "$path" "$DESTINATION_PATH"
+    rsync -avz --progress --relative "$path" "$DESTINATION_PATH"
 done
 
-echo
-echo "Sync completed successfully."
-echo
+# Get size of destination path after sync
+destination_size_after=$(get_destination_size)
 
-df -h
+# Calculate size of data synced during the current run
+current_sync_size=$((destination_size_after - destination_size_before))
+
+# Get current date
+sync_datetime=$(date "+%Y-%m-%d %H:%M:%S")
+
+# Create JSON data
+json_data=$(cat <<EOF
+{
+  "time_of_sync": "$sync_datetime",
+  "destination_path_size": $destination_size_after,
+  "current_sync_size": $current_sync_size
+}
+EOF
+)
+
+# Write JSON data to file
+echo "$json_data" > sync_info.json
+
+echo "Sync completed successfully."
